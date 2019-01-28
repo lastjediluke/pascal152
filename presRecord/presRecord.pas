34a0d7f3f006922c015f8e1152713af116f8a422
lastjediluke <lukeisaboss96@yahoo.com>
2019-01-28 13:50:49 -0800

    pres record is done, son

--- presRecord/presRecord.pas
+++ presRecord/presRecord.pas
@@ -8,35 +8,43 @@ type
 
 type
 	presidentRecord = record
-		p_id : names;
+		p_id : integer;
 		p_fname : names;
 		p_lname : names;
 	end;
+
 type
 	presArray = array[1..12] of presidentRecord;
 
+type
+	searchArray = array[1..5] of integer;
+
+type
+	numbers = '0'..'9';
+	numbersSet = set of numbers;
+
 var 
 	f : file of char;
-	myFile : file of char;
 	myArray : ch_array;
 	i : integer;
-	idCounter : integer;
-	fnameCounter : integer;
-	lnameCounter : integer;
-	myRecord : presidentRecord;
 	myPresArray : presArray;
+	paCount : integer;
+
 	
-
-
 function getId(var a : ch_array; var pa : presidentRecord; i : integer) : integer;
+var
+	idCount : integer;
 begin
+	idCount := 1;
 	while a[i] <> ' ' do
 	begin
-		pa.p_id[i] := a[i];
+		pa.p_id := 10*pa.p_id + (ord(a[i]) - ord('0'));
 		i := i + 1;
+		idCount := idCount + 1;
 	end;
 	getId := i + 1;
 end;
+
 
 function getFname(var a : ch_array; var pa : presidentRecord; i : integer) : integer;
 var 
@@ -49,8 +57,10 @@ begin
 		i := i + 1;
 		fCount := fCount + 1;
 	end;
+
 	getFname := i + 1;
 end;
+
 
 function getLname(var a : ch_array; var pa : presidentRecord; i : integer) : integer;
 var 
@@ -63,36 +73,105 @@ begin
 		i := i + 1;
 		lCount := lCount + 1;
 	end;
-	getlname := i + 1;
+	getLname := i + 1;
 end;
 
-procedure fillRecord(var a : ch_array; var pa : presArray);
+{ * ideally, this function should split into 2 different functions * }
+function presidentSearch(var a : ch_array; var pa : presArray; i : integer) : boolean;
+var
+	intToSearch : searchArray;
+	intsCount : integer;
+	presArrCount : integer;
+	myNums : numbersSet;
+	c : numbers;
+begin
+	c := a[i];
+	intsCount := 1;
+	writeln('Searching...');
+	myNums := ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
+	{ * init the search items array *}
+	while intsCount <> 6 do
+	begin
+		intToSearch[intsCount] := 0;
+		intsCount := intsCount + 1;
+	end;
+	intsCount := 1;
+
+	{ * find search items and store into intToSearch array * }
+	while true do
+	begin
+		c := a[i];
+		if c in myNums then
+		begin
+			intToSearch[intsCount] := (10*intToSearch[intsCount]) + (ord(c) - ord('0'));
+			i := i + 1;
+		end
+		
+		else if a[i] = ' ' then
+		begin
+			i := i + 1;
+			intsCount := intsCount + 1;
+		end
+
+		else begin
+			break
+		end;
+	end;
+	presArrCount := 1;
+	intsCount := 1;
+
+	{ * search through my president records to find matches* }
+	while intToSearch[intsCount] <> 0 do 
+	begin
+		while true do
+		begin
+			if pa[presArrCount].p_id = 0 then
+			begin
+				writeln('No Match for ', intToSearch[intsCount]);
+				break;
+			end;
+
+			if pa[presArrCount].p_id = intToSearch[intsCount] then
+			begin
+				write('Match! ');
+				write(pa[presArrCount].p_fname);
+				write(' ');
+				writeln(pa[presArrCount].p_lname);
+				break;
+			end;
+			presArrCount := presArrCount + 1;
+		end;
+		intsCount := intsCount + 1;
+		presArrCount := 1;
+	end;	
+	presidentSearch := true;
+end;
+
+procedure printPresidents(var pa : presArray);
 var
 	i : integer;
-	paCount : integer;
 begin
 	i := 1;
-	paCount := 1;
-	while a[i] <> '-' do
+	writeln('Printing...');
+	while pa[i].p_id <> 0 do
 	begin
-		i := getId(a, presArray[paCount], i);
-
-		i := getFname(a, presArray[paCount], i);
-	
-		i := getLname(a, presArray[paCount], i);
-		paCount := paCount + 1;
+		write(pa[i].p_id);
+		write(' ');
+		write(pa[i].p_fname);
+		write(' ');
+		writeln(pa[i].p_lname);
+		i := i + 1;
 	end;
-	writeln(presArray);
-
 end;
 
-
-
+{ */// MAIN \\\* }
 begin
 	i := 1;
 	assign(f, 'record.txt');
 	reset(f);
+
+	{ * read from a file into a character array * }
 	while not eof(f) do
 	begin
 		read(f, myArray[i]);
@@ -100,21 +179,21 @@ begin
 	end;
 	close(f);
 	
-	writeln(myArray);
-
+	{ * I would have liked to put while loop below into a function, however, I kept getting errors * }
 	i := 1;
-	idCounter := 1;
-	fnameCounter := 1;
-	lnameCounter := 1;
-	
-	fillRecord(myArray, myPresArray);
+	paCount := 1;
 
-	{
-	while not eof(myArray) do
+	{ * fill up the record from myArray * }
+	while myArray[i] <> '-' do
 	begin
-		write(myArray[i]);
-		i := i + 1;
+		i := getId(myArray, myPresArray[paCount], i);
+		i := getFname(myArray, myPresArray[paCount], i);
+		i := getLname(myArray, myPresArray[paCount], i);
+		paCount := paCount + 1;
 	end;
-	}
 
+	{ * i should = - at this point, so I need to add 3 to get it to the search items * }
+	i := i + 3;
+	presidentSearch(myArray, myPresArray, i);
+	printPresidents(myPresArray);	
 end.
